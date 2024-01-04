e7c21d19b7a2508e48b777fcbe80fce9cd6c6c0a
n4j1Br4ch1D <kawner93@gmail.com>
2024-01-04 14:21:19 +0100

    init project

---
+++ src/main/java/com/anmoon/mentionbox/MentionboxApplication.java
@@ -0,0 +1,13 @@
+package com.anmoon.mentionbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentionboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MentionboxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anmoon/mentionbox/MentionboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anmoon.mentionbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentionboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
